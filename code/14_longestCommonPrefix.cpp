6c0b63477697911aa9bb9fb41a3175af265c5675
bobby77777 <bobby7892143@gmail.com>
2022-10-20 11:48:45 +0800

    finish 14

--- code/14_longestCommonPrefix.cpp
+++ code/14_longestCommonPrefix.cpp
@@ -6,6 +6,24 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+        int ans = strs[0].length(), n = strs.size();
+        for(int i = 1; i < n; i++){
+            int j = 0;
+            while(j < strs[i].length() && strs[i][j] == strs[0][j])
+                j++;
+            ans = min(ans, j);
+        }
+        return strs[0].substr(0, ans);
     }
 };
+
+int main() {
+    vector<string> strs;
+    strs.push_back("flower");
+    strs.push_back("flow");
+    strs.push_back("flight");
+    // cout << strs[0].substr(0, 0) << endl;
+    Solution sol;
+    cout << sol.longestCommonPrefix(strs) << endl;
+    return 0;
+}
