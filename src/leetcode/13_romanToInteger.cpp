e1839a0afffef077c6aa88710620f011c65f3d24
bobby77777 <bobby7892143@gmail.com>
2026-05-06 16:23:33 +0800

    finish 13

---
+++ src/leetcode/13_romanToInteger.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    public:
+        int romanToInt(string s) {
+            unordered_map<char, int> map = {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+            
+            int result = 0;
+            
+            for (size_t i = 0; i < s.size(); i++) {
+                if (map[s[i]] < map[s[i+1]]) {
+                    result -= map[s[i]];
+                } else {
+                    result += map[s[i]];
+                }
+            }
+
+            return result;
+        }
+};
+
+int main() {
+    Solution solution;
+    string s = "LVIII";
+    cout << solution.romanToInt(s) << endl;
+    return 0;
+}
